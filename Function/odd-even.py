d2f2077e45210bc2372d0f283a400ebd05f53328
VanshikaVb <128689989+VanshikaVb@users.noreply.github.com>
2023-04-28 11:51:27 +0530

    Create odd-even.py

---
+++ Function/odd-even.py
@@ -0,0 +1,14 @@
+def lesser_of_two_evens(a,b):
+    if a%2 == 0 and b%2 ==0 :
+        if(a < b):
+            result = a
+        else:
+            result = b
+         
+    else:
+        if(a>b):
+            result = a
+        else:
+            result = b
+    
+    return result
